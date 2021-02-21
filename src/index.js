06a944ed915911c0da40fd97cf8b3b73d2043f61
Mario <mario.sandoval@freeagentsoftware.com>
2021-02-21 15:46:33 -0600

    Initial App Structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom'; 
+import ReactDOM from 'react-dom'; 
+import { BrowserRouter as Router} from 'react-router-dom';
 
-function Hi(){
-    return <p>Hi.</p>
-}
+import App from './components/App';
 
-render (<Hi/>, document.getElementById("app"));
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+ReactDOM.render (
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById("app")
+);
